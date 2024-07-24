ec6f0befb9300436e52ace28a725498bea5e0b23
rex <jimingotjams557@gmail.comm>
2024-07-24 15:07:32 +0530

    Change the file

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
-import { defineConfig } from 'vite';
-import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+
 export default defineConfig({
-  plugins: [
-    // Add Tailwind CSS plugin
-    {
-      name: 'tailwindcss',
-      config() {
-        return {
-          css: {
-            postcss: {
-              plugins: [tailwindcss()],
-            },
-          },
-        };
-      },
-    },
-  ],
-});
+  build: {
+    outDir: 'dist',
+  },
+})
